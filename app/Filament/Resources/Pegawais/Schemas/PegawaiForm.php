df0d6043ef39d6a6469d975385b37a00751d8dee
yogi-maulana-dev <yogi.maulana.2021211032@mail.darmajaya.ac.id>
2026-03-30 20:43:57 +0700

    bug pegawai

--- app/Filament/Resources/Pegawais/Schemas/PegawaiForm.php
+++ app/Filament/Resources/Pegawais/Schemas/PegawaiForm.php
@@ -2,15 +2,52 @@
 
 namespace App\Filament\Resources\Pegawais\Schemas;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class PegawaiForm
 {
     public static function configure(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                //
-            ]);
+        return $schema->schema([
+            TextInput::make('nip')
+                ->required()
+                ->unique(ignoreRecord: true),
+
+            TextInput::make('nama')
+                ->required(),
+
+            DatePicker::make('tanggal_lahir'),
+
+            Select::make('jenis_kelamin')
+                ->options([
+                    'L' => 'Laki-laki',
+                    'P' => 'Perempuan',
+                ]),
+
+            TextInput::make('alamat'),
+            TextInput::make('no_hp'),
+            TextInput::make('email'),
+
+            Select::make('id_jabatan')
+                ->relationship('jabatan', 'nama_jabatan')
+                ->required(),
+
+            Select::make('id_unit')
+                ->relationship('unit', 'nama_unit')
+                ->required(),
+
+            Select::make('id_status')
+                ->relationship('status', 'nama_status')
+                ->required(),
+
+            Select::make('id_golongan')
+                ->relationship('golongan', 'nama_golongan')
+                ->required(),
+
+            DatePicker::make('tanggal_masuk'),
+        ]);
     }
 }
--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -2,10 +2,98 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pegawai extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'pegawais';
     protected $primaryKey = 'id_pegawai';
+
+    protected $fillable = [
+        'nip',
+        'nama',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'alamat',
+        'no_hp',
+        'email',
+        'id_jabatan',
+        'id_unit',
+        'id_status',
+        'id_golongan',
+        'tanggal_masuk',
+    ];
+
+    // RELASI MASTER
+    public function jabatan()
+    {
+        return $this->belongsTo(Jabatan::class, 'id_jabatan', 'id_jabatan');
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'id_unit', 'id_unit');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'id_status', 'id_status');
+    }
+
+    public function golongan()
+    {
+        return $this->belongsTo(Golongan::class, 'id_golongan', 'id_golongan');
+    }
+
+    // RELASI TRANSAKSI
+    public function absensi()
+    {
+        return $this->hasMany(Absensi::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    public function gaji()
+    {
+        return $this->hasMany(Gaji::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    public function cuti()
+    {
+        return $this->hasMany(Cuti::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    public function sk()
+    {
+        return $this->hasMany(Sk::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    public function riwayatGaji()
+    {
+        return $this->hasMany(RiwayatGaji::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    public function riwayatJabatan()
+    {
+        return $this->hasMany(RiwayatJabatan::class, 'id_pegawai', 'id_pegawai');
+    }
+
+    // LOGIKA KENAIKAN GAJI 3 TAHUN
+    public function layakNaikGaji(): bool
+    {
+        $skTerakhir = $this->sk()
+            ->where('jenis_sk', 'Kenaikan Gaji')
+            ->latest('tanggal_sk')
+            ->first();
+
+        if (!$skTerakhir) {
+            return true;
+        }
+
+        return Carbon::parse($skTerakhir->tanggal_sk)
+            ->addYears(3)
+            ->lte(Carbon::now());
+    }
 }
